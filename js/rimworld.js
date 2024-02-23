40e6be67234b529e44032f3931f924a3a8a7c438
Jerry Johnson <jjj21393@gmail.com>
2024-02-22 22:27:24 -0500

    add meal types

--- js/rimworld.js
+++ js/rimworld.js
@@ -1,54 +1,49 @@
 function calculateNutrition() {
-  const input = $(".numberOfColonistsInput");
-  const numberOfColonists = input.val();
-
+  const numberOfColonists = $(".numberOfColonistsInput").val();
   const nutritionRequiredPerDay = 1.6 * numberOfColonists;
 
-  riceCalc(nutritionRequiredPerDay);
-  cornCalc(nutritionRequiredPerDay);
-  potatoCalc(nutritionRequiredPerDay);
-  berryCalc(nutritionRequiredPerDay);
-  fungusCalc(nutritionRequiredPerDay);
+  const mealFactor = getMealFactor();
+
+  plantCalc(nutritionRequiredPerDay, mealFactor, "ricePlantsNeeded", 0.054);
+  plantCalc(nutritionRequiredPerDay, mealFactor, "cornPlantsNeeded", 0.0525);
+  plantCalc(nutritionRequiredPerDay, mealFactor, "potatoPlantsNeeded", 0.0515);
+  plantCalc(nutritionRequiredPerDay, mealFactor, "berryPlantsNeeded", 0.047);
+  plantCalc(nutritionRequiredPerDay, mealFactor, "fungusPlantsNeeded", 0.0495);
 }
 
-function riceCalc(nutritionRequiredPerDay) {
-  const riceNutritionPerDayPerPlant = 0.054;
-  const ricePlantsNeeded =
-    nutritionRequiredPerDay / riceNutritionPerDayPerPlant;
+function getMealFactor() {
+  const mealOption = document.querySelector(
+    'input[name="foodSelect"]:checked'
+  ).value;
 
-  placeOutput("ricePlantsNeeded", ricePlantsNeeded);
+  let multiplier;
+  if (mealOption === "raw") {
+    multiplier = 1;
+  } else if (mealOption === "paste") {
+    multiplier = 3;
+  } else if (mealOption === "simple") {
+    multiplier = 1.8;
+  } else if (mealOption === "fine") {
+    multiplier = 1.8;
+  } else if (mealOption === "lavish") {
+    multiplier = 1;
+  }
+
+  return multiplier;
 }
 
-function cornCalc(nutritionRequiredPerDay) {
-  const cornNutritionPerDayPerPlant = 0.0525;
-  const cornPlantsNeeded =
-    nutritionRequiredPerDay / cornNutritionPerDayPerPlant;
+function plantCalc(
+  nutritionRequiredPerDay,
+  mealFactor,
+  elementClass,
+  plantNutritonPerPlantPerDay
+) {
+  const nutritionPerPlantAfterCooking =
+    plantNutritonPerPlantPerDay * mealFactor;
 
-  placeOutput("cornPlantsNeeded", cornPlantsNeeded);
-}
+  const plantsNeeded = nutritionRequiredPerDay / nutritionPerPlantAfterCooking;
 
-function potatoCalc(nutritionRequiredPerDay) {
-  const potatoNutritionPerDayPerPlant = 0.0515;
-  const potatoPlantsNeeded =
-    nutritionRequiredPerDay / potatoNutritionPerDayPerPlant;
-
-  placeOutput("potatoPlantsNeeded", potatoPlantsNeeded);
-}
-
-function berryCalc(nutritionRequiredPerDay) {
-  const berryNutritionPerDayPerPlant = 0.047;
-  const berryPlantsNeeded =
-    nutritionRequiredPerDay / berryNutritionPerDayPerPlant;
-
-  placeOutput("berryPlantsNeeded", berryPlantsNeeded);
-}
-
-function fungusCalc(nutritionRequiredPerDay) {
-  const fungusNutritionPerDayPerPlant = 0.0495;
-  const fungusPlantsNeeded =
-    nutritionRequiredPerDay / fungusNutritionPerDayPerPlant;
-
-  placeOutput("fungusPlantsNeeded", fungusPlantsNeeded);
+  placeOutput(elementClass, plantsNeeded);
 }
 
 function placeOutput(htmlClass, number) {

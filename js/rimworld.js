92d675d9f0b12741ff1251d01e33123fc4ab0e65
Jerry Johnson <jjj21393@gmail.com>
2024-02-22 22:40:37 -0500

    refactor plant calc code

--- js/rimworld.js
+++ js/rimworld.js
@@ -1,52 +1,37 @@
-function calculateNutrition() {
+const calculateNutrition = () => {
   const numberOfColonists = $(".numberOfColonistsInput").val();
   const nutritionRequiredPerDay = 1.6 * numberOfColonists;
 
-  const mealFactor = getMealFactor();
-
-  plantCalc(nutritionRequiredPerDay, mealFactor, "ricePlantsNeeded", 0.054);
-  plantCalc(nutritionRequiredPerDay, mealFactor, "cornPlantsNeeded", 0.0525);
-  plantCalc(nutritionRequiredPerDay, mealFactor, "potatoPlantsNeeded", 0.0515);
-  plantCalc(nutritionRequiredPerDay, mealFactor, "berryPlantsNeeded", 0.047);
-  plantCalc(nutritionRequiredPerDay, mealFactor, "fungusPlantsNeeded", 0.0495);
-}
-
-function getMealFactor() {
   const mealOption = document.querySelector(
     'input[name="foodSelect"]:checked'
   ).value;
 
-  let multiplier;
-  if (mealOption === "raw") {
-    multiplier = 1;
-  } else if (mealOption === "paste") {
-    multiplier = 3;
-  } else if (mealOption === "simple") {
-    multiplier = 1.8;
-  } else if (mealOption === "fine") {
-    multiplier = 1.8;
-  } else if (mealOption === "lavish") {
-    multiplier = 1;
-  }
+  plantCalc(nutritionRequiredPerDay, mealOption, "ricePlantsNeeded", 0.054);
+  plantCalc(nutritionRequiredPerDay, mealOption, "cornPlantsNeeded", 0.0525);
+  plantCalc(nutritionRequiredPerDay, mealOption, "potatoPlantsNeeded", 0.0515);
+  plantCalc(nutritionRequiredPerDay, mealOption, "berryPlantsNeeded", 0.047);
+  plantCalc(nutritionRequiredPerDay, mealOption, "fungusPlantsNeeded", 0.0495);
+};
 
-  return multiplier;
-}
-
-function plantCalc(
+const plantCalc = (
   nutritionRequiredPerDay,
-  mealFactor,
+  mealOption,
   elementClass,
   plantNutritonPerPlantPerDay
-) {
+) => {
   const nutritionPerPlantAfterCooking =
-    plantNutritonPerPlantPerDay * mealFactor;
+    plantNutritonPerPlantPerDay * mealMultiplerMap[mealOption];
 
   const plantsNeeded = nutritionRequiredPerDay / nutritionPerPlantAfterCooking;
 
-  placeOutput(elementClass, plantsNeeded);
-}
+  const output = $(`.${elementClass}`);
+  output.html(Math.round(plantsNeeded));
+};
 
-function placeOutput(htmlClass, number) {
-  const output = $(`.${htmlClass}`);
-  output.html(Math.round(number));
-}
+const mealMultiplerMap = {
+  raw: 1,
+  paste: 3,
+  simple: 1.8,
+  fine: 1.8,
+  lavish: 1,
+};

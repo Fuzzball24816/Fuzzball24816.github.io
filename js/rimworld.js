3b394b6280d262121f5d1efe0b89c74366b87a36
Jerry Johnson <jjj21393@gmail.com>
2024-02-27 23:43:51 -0500

    add more skils, and global work speed
    Co-authored-by: Nicholas Nelson <OregonNelson@gmail.com>

--- js/rimworld.js
+++ js/rimworld.js
@@ -38,14 +38,64 @@ const mealMultiplerMap = {
 
 const masterFunction = () => {
   updateConstruction();
+  updateCooking();
+  updateMining();
+  updatePlants();
 };
 
 const updateConstruction = (inputs) => {
   const contructionSkill = Number(
     document.getElementById("constructionInput").value
   );
-  const constructionTime = ((0.3 + 0.0875 * contructionSkill) * 100).toFixed(2);
-  const constructionSpeed = document.getElementById("constructionSpeed");
 
-  constructionSpeed.innerText = constructionTime;
+  let constructionMultiplier = 0.3 + 0.0875 * contructionSkill;
+
+  constructionMultiplier = applyGlobalWorkSpeed(constructionMultiplier);
+
+  const constructionSpeed = document.getElementById("constructionSpeed");
+  constructionSpeed.innerText = (constructionMultiplier * 100).toFixed(0);
+};
+
+const updateCooking = (inputs) => {
+  const cookingSkill = Number(document.getElementById("cookingInput").value);
+  let cookingMultiplier = 0.4 + 0.06 * cookingSkill;
+
+  cookingMultiplier = applyGlobalWorkSpeed(cookingMultiplier);
+
+  const cookingSpeed = document.getElementById("cookingSpeed");
+  cookingSpeed.innerText = (cookingMultiplier * 100).toFixed(0);
+};
+
+const updateMining = (inputs) => {
+  const miningSkill = Number(document.getElementById("miningInput").value);
+  let miningMultiplier = 0.04 + 0.12 * miningSkill;
+
+  miningMultiplier = applyGlobalWorkSpeed(miningMultiplier);
+
+  const miningSpeed = document.getElementById("miningSpeed");
+  miningSpeed.innerText = (miningMultiplier * 100).toFixed(0);
+};
+
+const updatePlants = (inputs) => {
+  const plantsSkill = Number(document.getElementById("plantsInput").value);
+  let plantsMultiplier = 0.08 + 0.115 * plantsSkill;
+
+  plantsMultiplier = applyGlobalWorkSpeed(plantsMultiplier);
+
+  if (plantsMultiplier < 0.1) {
+    plantsMultiplier = 0.1;
+  }
+
+  const plantsSpeed = document.getElementById("plantsSpeed");
+  plantsSpeed.innerText = (plantsMultiplier * 100).toFixed(0);
+};
+
+const applyGlobalWorkSpeed = (speed) => {
+  let globalWorkSpeed = Number(
+    document.getElementById("globalWorkSpeedInput").value
+  );
+
+  globalWorkSpeed /= 100;
+  speed *= globalWorkSpeed;
+  return speed;
 };

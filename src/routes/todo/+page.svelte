18a941d584ea443c6e3ec75120325665f6f44c22
Jerry Johnson <jjj21393@gmail.com>
2024-03-20 01:04:12 -0400

    improve todos styling, add wip edit button
    Co-authored-by: Nicholas <OregonNelson@users.noreply.github.com>

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -1,39 +1,75 @@
 <script>
-  let todos = [];
+  let todos = [
+    { text: "todo 1", isEditing: false },
+    { text: "todo 2", isEditing: false },
+    { text: "todo 3", isEditing: false },
+  ];
+
   let todoInput;
+
+  const addTodo = () => {
+    if (todoInput) {
+      const newTodo = {
+        text: todoInput,
+        isEditing: false,
+      };
+      todos = [...todos, newTodo];
+    }
+  };
+
+  const deleteTodo = (indexToDelete) => {
+    todos = [
+      ...todos.slice(0, indexToDelete),
+      ...todos.slice(indexToDelete + 1),
+    ];
+  };
+
+  const editTodo = (indexToEdit) => {
+    todos[indexToEdit].isEditing = true;
+  };
+
+  const saveTodo = (indexToSave) => {
+    todos[indexToSave].isEditing = false;
+  };
+
+  const deleteAllTodos = () => {
+    todos = [];
+  };
 </script>
 
 <h1>Todo List</h1>
-<input bind:value={todoInput} />
-<button
-  class="addElementButton"
-  on:click={() => {
-    if (todoInput) todos = [...todos, todoInput];
-  }}>Add Element</button
->
-<button class="deleteAllButton" on:click={() => (todos = [])}>
-  Delete All
-</button>
+<form on:submit|preventDefault={addTodo}>
+  <input bind:value={todoInput} />
+  <button class="addElementButton">Add Element</button>
+</form>
+<button class="deleteAllButton" on:click={deleteAllTodos}>Delete All</button>
 
 {#if todos.length > 0}
-  <div class="todoBackground">
+  <div class="todoContainer">
     {#each todos as todo, i}
-      <div class="listElement">{todo}</div>
-      <button
-        class="deleteButton"
-        on:click={() => (todos = [...todos.slice(0, i), ...todos.slice(i + 1)])}
-        >Delete Button</button
-      >
+      <div>
+        {#if todo.isEditing}
+          <input />
+          <button on:click={() => saveTodo(i)}>Save</button>
+        {:else}
+          <span>{todo.text}</span>
+          <button on:click={() => editTodo(i)}>Edit</button>
+        {/if}
+        <button on:click={() => deleteTodo(i)}> Delete </button>
+      </div>
     {/each}
   </div>
 {/if}
 
 <style>
-  .todoBackground {
-    background-color: black;
+  .todoContainer {
+    background-color: #0000009c;
     border-radius: 5px;
     text-align: center;
     padding: 15px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
   }
 
   h1 {
